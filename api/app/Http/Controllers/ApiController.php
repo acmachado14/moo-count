bb914747d06113e376dd9662eea714ada3ad4caf
Angelo <angelo.machado@ufv.br>
2023-11-27 09:18:34 -0300

    :recycle: refactor: Change api request to predictions storage

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Predictions;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     public function handlePost(Request $request)
     {
-        $postData = $request->all();
-        // ou $postData = $request->input(); dependendo da sua preferência
-        // Faça o que precisar com os dados postados, por exemplo, salvar no banco de dados ou processar.
+        $request->validate([
+            'quantity' => 'required',
+            'local' => 'required',
+            'user_email' => 'required'
+        ]);
 
-        // Responder com uma mensagem simples
-        return response()->json(['message' => 'POST data received successfully!']);
+        $user = User::where('email', $request['user_email'])->first();
+
+        if (!isset($user)){
+            return response()->json(['message' => 'No user found with this email!'], 404);
+        }
+
+        $prediction = Predictions::create([
+            'quantity' => $request['quantity'],
+            'local' =>  $request['local'],
+            'user_id' => $user->id
+        ]);
+
+        return response()->json(['message' => 'Predicion storage successfully!']);
     }
 }
